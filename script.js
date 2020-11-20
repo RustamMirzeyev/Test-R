cfb6b3a3ed1a14e089c896f6b7c08d3497f13fef
Rustam Mirzoev <74654492+RustamMirzeyev@users.noreply.github.com>
2020-11-20 16:00:17 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,120 @@
+// var age = 55 
+
+// //  if(age >= 18)
+// // {
+// // if(age >=50)
+// // {
+// // console.log("belede")
+// // }
+// // else {
+// //     console.log ("heeeeeeee")
+// // }
+
+// // // console.log ("heeeeeeee")
+
+// // }
+// // else {
+// //     console.log("yoxxxxxxxxxxxx")
+// // }
+
+
+// // console.log (age >= 15  ? age > 50  ? "bele de" : "heeeeeee" : "Yoxxxxxxxx");
+// var a =15;
+// var b = 25;
+
+// if (!(a > 30 || b< a && 33 < 29))
+// {
+// console.log("False oldu ama isledi ");
+// }
+
+// var  sqiymeti = 25 
+// var iqiymeti = 60 ;
+//  if (sqiymeti >= 0 && sqiymeti <=100 && iqiymeti >=0 && iqiymeti <=100)
+//  {
+// if (sqiymeti/2 + iqiymeti/2 >=50)
+// {
+//     console.log ("qonaqliq");
+
+// }
+
+// else {
+
+//     console.log ("25 faiz" );
+
+// }
+// }
+// else {
+//     console.log ("olmaz")
+// }
+
+// var num = 11
+// while (num <=10 )
+// {
+
+//     num++;
+//     console.log(num);
+
+
+// }
+// do {
+// num++;
+//     console.log(num);
+
+// }
+// while(num <=10)
+
+// for(var num = 0 ; num <=10 ; num++)
+// {
+
+//     console.log (num);
+// }
+
+
+
+// function Dark(n, m) {
+
+
+//     while (n <= m) {
+//         n++;
+//         console.log(n)
+//     }
+// }
+// Dark(1, 10);
+
+
+// function Olsun (n)
+// {
+// fo
+
+// if (n%3==0 && n%5==0 && n%15==0)
+// {
+
+
+// }
+
+
+// }
+
+
+function fizzbuzz() {
+    var n = 0;
+    while (n <= 100) {
+        if (n % 15 == 0)
+            console.log("fizzbuzz");
+        else if (n % 3 == 0)
+            console.log("fizz");
+        else if (n % 5 == 0)
+            console.log(" buzz")
+        else
+
+            console.log(n)
+
+
+
+        n++;
+    }
+}
+
+
+
+fizzbuzz()
